69445d3254327529dc857ba523997affbc3af32e
CameronHoying <143430904+CameronHoying@users.noreply.github.com>
2023-10-22 13:56:38 -0500

    Add files via upload

---
+++ matchmaker.js
@@ -0,0 +1,62 @@
+function CalculateCompatability () {
+
+    const RESPONSE = [
+        5, /* Strongly Agree */
+        4, /* Agree */
+        3, /* Neutral */
+        2, /* Disagree */
+        1, /* Strongly Disagree */
+    ]
+    const SCORE = 25;
+    let Question1Answer = document.getElementById("question1").selectedOptions[0].value;
+    let Question2Answer = document.getElementById("question2").selectedOptions[0].value;
+    let Question3Answer = document.getElementById("question3").selectedOptions[0].value;
+    let Question4Answer = document.getElementById("question4").selectedOptions[0].value;
+    let Question5Answer = document.getElementById("question5").selectedOptions[0].value;
+
+    console.log(document.getElementById("question1").selectedOptions[0].text);
+    console.log(document.getElementById("question1").selectedOptions[0].value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
